5e64fcebec1bb37f8ab7b8d22771783267e547dc
Gabriel Brito de Oliveira <gabriel63235036@edu.df.senac.br>
2026-09-08 17:40:42 -0300

    att

---
+++ Script/calendario.js
@@ -0,0 +1,6 @@
+const monthElement = document.getElementById("month");
+const yearElement = document.getElementById("year");
+const calendarElement = document.getElementById("calendar");
+
+const previousMonthButton = document.getElementById("previousMonth");
+const nextMonthButton = document.getElementById("nextMonth");
